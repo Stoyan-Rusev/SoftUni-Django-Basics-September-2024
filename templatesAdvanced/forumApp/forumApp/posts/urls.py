2705953658a3172469da9bdc8130e5360cd8686d
Stoyan-Rusev <stoyan.rusevv@gmail.com>
2024-10-11 21:04:04 +0300

    Add files via upload
    Exercise on template inheritance and custom tags and filters for the forumApp.

---
+++ templatesAdvanced/forumApp/forumApp/posts/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from forumApp.posts import views
+
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('dashboard/', views.dashboard_view, name='dash'),
+    path('add-post/', views.add_post, name='add-post'),
+    path('<int:pk>/', include([
+        path('delete-post/', views.delete_post, name='delete-post'),
+        path('details-post/', views.details_page, name='details-post'),
+        path('edit-post/', views.edit_post, name='edit-post'),
+    ])),
+    path('test/', views.test, name='test'),
+]
